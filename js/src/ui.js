c078811c18cd1064c716321045df543ad1ae3029
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-04-13 15:22:59 +0100

    HS-2192 added steps architecture

--- js/src/ui.js
+++ js/src/ui.js
@@ -12,6 +12,52 @@
 		stepsKeys = _.keys(steps),
 		activeStep = 0;
 
-	debug.info(stepsKeys);
+	var section = {
+		active: stepActive,
+		next: stepNext,
+		prev: stepPrev
+	};
+
+	assignActive();
+
+	function assignActive() {
+		var vProgress = $("#vProgress");
+		var children = vProgress.children();
+
+		_.forEach(children, function(ele) {
+			console.log(section.active());
+			var toggleSection = $(ele).attr("toggle-section");
+			toggleSection == section.active() ? $(ele).find("a").addClass("active") : "";
+		});
+	}
+
+	$(".categorytile").click(function() {
+		section.next();
+		console.log(activeStep);
+		//alert("ok");
+	});
+
+	function stepActive() {
+		return stepsKeys[activeStep];
+	}
+
+	function stepNext() {
+		// @TODO document why 2 rather than 1!
+		if (activeStep > (stepsKeys.length - 2)) {
+			return false;
+		}
+
+		activeStep = activeStep + 1;
+		assignActive();
+	}
+
+	function stepPrev() {
+		if (activeStep < 1) {
+			return false;
+		}
+
+		activeStep = activeStep - 1;
+		assignActive();
+	}
 
 })();
