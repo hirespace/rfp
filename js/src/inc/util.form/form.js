dcf29e22699993e8473ee4ff99cee59a07d75f0b
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-05-14 16:46:14 +0100

    HS-2448 prototype WIP, replaced regex

--- js/src/inc/util.form/form.js
+++ js/src/inc/util.form/form.js
@@ -36,7 +36,7 @@
 			return (/^("([ !\x23-\x5B\x5D-\x7E]*|\\[ -~])+"|[-a-z0-9!#$%&'*+\/=?^_`{|}~]+(\.[-a-z0-9!#$%&'*+\/=?^_`{|}~]+)*)@([0-9a-z\u00C0-\u02FF\u0370-\u1EFF]([-0-9a-z\u00C0-\u02FF\u0370-\u1EFF]{0,61}[0-9a-z\u00C0-\u02FF\u0370-\u1EFF])?\.)+[a-z\u00C0-\u02FF\u0370-\u1EFF][-0-9a-z\u00C0-\u02FF\u0370-\u1EFF]{0,17}[a-z\u00C0-\u02FF\u0370-\u1EFF]$/i).test(val);
 		},
 		number: function (val) {
-			return (/^-?[0-9]+$/).test(val);
+			return (/^(\d{1,3}(,\d{3})*(\.\d\d)?|\.\d\d)$/).test(val);
 		},
 		tel: function(val) {
 			return (/^[\(\)\s\-\+\d]{10,17}$/).test(val);
